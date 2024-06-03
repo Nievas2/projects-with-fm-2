45ba0cdfcea12794624abb80a3b07ad115f13620
Nievas2 <angelgabrielnievas@gmail.com>
2024-06-03 15:01:15 -0300

    feat(job-filtering): :sparkles: add filter

--- projects/05-job-filtering/src/app/page.tsx
+++ projects/05-job-filtering/src/app/page.tsx
@@ -1,15 +1,81 @@
+"use client"
 import Image from "next/image"
 import data from "../assets/data.json"
 import Card from "@/components/card"
+import { useContext, useEffect, useState } from "react"
 export default function Home() {
+  const [languages, setLanguages] = useState<string[]>([])
+  const [dataFiltered, setDataFiltered] = useState(data)
+  console.log("remder")
+  useEffect(() => {
+    console.log(languages)
+
+    if (languages.length == 0) {
+      setDataFiltered(data)
+    } else {
+      const dataFilt = data.filter((proyect) =>
+        proyect.languages.some((language) => languages.includes(language))
+      )
+      setDataFiltered(dataFilt)
+    }
+  }, [languages])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center pt-9">
-      {data.map((card) => (
-        <Card
-          key={crypto.randomUUID()}
-          data={card}
+    <main className="flex h-screen flex-col items-center justify-center">
+      <section className="bg-[#5ba4a4]  w-full">
+        <img
+          className="hidden sm:block h-full"
+          src="/bg-header-desktop.png"
+          alt="header image"
         />
-      ))}
+        <img
+          className="flex sm:hidden w-full"
+          src="/bg-header-mobile.svg"
+          alt="header image"
+        />
+      </section>
+      <section
+        className={`flex gap-5 bg-white shadow-xl content-center p-2 rounded-md relative mt-[-25px] ${
+          languages.length > 0 ? "block" : "hidden"
+        }`}
+      >
+        {languages &&
+          languages.map((data: string) => (
+            <div
+              key={crypto.randomUUID()}
+              className={`m-1 bg-[#effafa] rounded-sm`}
+            >
+              <span className="px-1 text-sm">{data}</span>
+              <button
+                className="bg-[#5ba4a4] p-2 rounded-r-sm"
+                onClick={() =>
+                  setLanguages(languages.filter((item) => item !== data))
+                }
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                >
+                  <path
+                    fill="#FFF"
+                    fillRule="evenodd"
+                    d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          ))}
+      </section>
+      <section className="pt-9 flex items-center flex-col w-full gap-8 sm:gap-4 min-h-full">
+        {dataFiltered.map((card) => (
+          <Card
+            key={crypto.randomUUID()}
+            data={card}
+            setLanguages={setLanguages}
+            languages={languages}
+          />
+        ))}
+      </section>
     </main>
   )
 }
--- projects/05-job-filtering/src/components/card.tsx
+++ projects/05-job-filtering/src/components/card.tsx
@@ -1,29 +1,59 @@
+"use client"
 import { CardType } from "@/assets/types/card"
 import data from "../assets/data.json"
 import Image from "next/image"
-export default function Card({ data }: { data: CardType }) {
+import { useContext } from "react"
+
+export default function Card({
+  data,
+  languages,
+  setLanguages
+}: {
+  data: CardType
+  languages: string[]
+  setLanguages: any
+}) {
+  function handleChange(target: string) {
+    const result = languages.includes(target)
+    if (!result) {
+      const data = [...languages]
+      data.push(target)
+      setLanguages(data)
+      console.log(languages)
+    }
+  }
+
   return (
-    <main className="flex flex-row shadow-md w-3/4 p-4 gap-5">
-      <section className="basis-1/12 rounded-full flex items-center">
+    <main className="flex flex-col sm:flex-row shadow-xl rounded-md border-l-4 border-[#5ba4a4] h-30 w-3/4 p-4 gap-5">
+      <section className="sm:basis-2/12 rounded-full flex items-center">
         <div>
           <img
-          src={data.logo}
-          alt={data.company}
-          width={50}
-          height={50}
-        />
+            src={data.logo}
+            alt={data.company}
+            className={` w-[40px] h-[40px] mt-[-35px] sm:mt-0 sm:w-[80px] sm:h-[80px]`}
+          />
         </div>
-        
       </section>
-      <section className="flex basis-5/12 flex-col flex-grow gap-1">
+      <section className="flex sm:basis-3/12 flex-col flex-grow gap-1">
         <div className="flex flex-row gap-1 items-center">
-          <h1 className="text-[#5ba4a4] text-sm font-semibold">{data.company}</h1>
-          <span className="text-xs bg-[#5ba4a4] rounded-sm text-white px-2">
-            {data.new ? "NEW" : ""}
-          </span>
-          <span className="text-xs bg-[#1a1e2a] rounded-sm text-white px-2">
-            {data.featured ? "FEATURED" : ""}
-          </span>
+          <h1 className="text-[#5ba4a4] text-sm font-semibold">
+            {data.company}
+          </h1>
+          {data.new ? (
+            <span className="text-xs bg-[#5ba4a4] rounded-xl py-1 text-white px-2 ">
+              NEW
+            </span>
+          ) : (
+            ""
+          )}
+
+          {data.featured ? (
+            <span className="text-xs bg-[#1a1e2a] rounded-xl py-1 text-white px-2">
+              FEATURED
+            </span>
+          ) : (
+            ""
+          )}
         </div>
 
         <h2 className="font-extrabold">{data.position}</h2>
@@ -35,15 +65,18 @@ export default function Card({ data }: { data: CardType }) {
           <p className="font-extralight text-xs">{data.location}</p>
         </div>
       </section>
-      <section className="flex basis-6/12 flex-grow gap-5 align-middle justify-end">
-        {data.languages.map((language) => (
-          <span
-            key={crypto.randomUUID()}
-            className="text-sm bg-[#c9f7f7] rounded-xs p-2"
-          >
-            {language}
-          </span>
-        ))}
+      <section className="flex sm:basis-6/12 flex-grow gap-5 align-middle justify-normal sm:justify-end">
+        <div className="gap-5 content-center">
+          {data.languages.map((language) => (
+            <button
+              key={crypto.randomUUID()}
+              className="text-sm bg-[#effafa] rounded-sm px-2 py-1 ml-2"
+              onClick={() => handleChange(language)}
+            >
+              {language}
+            </button>
+          ))}
+        </div>
       </section>
     </main>
   )
